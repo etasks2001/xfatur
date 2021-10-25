5e462ab7751b188c66ff41c91646c3f93090bb80
satells <msergiost@hotmail.com>
2021-10-25 15:52:38 -0300

    start

---
+++ xfatur/src/main/java/com/xfatur/XFaturApplication.java
@@ -0,0 +1,13 @@
+package com.xfatur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XFaturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XFaturApplication.class, args);
+	}
+
+}
---
+++ xfatur/src/test/java/com/xfatur/XFaturApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfatur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XFaturApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
