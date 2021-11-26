b564d469fdec941650373796da06ec4523f993aa
satells <msergiost@hotmail.com>
2021-11-26 11:00:42 -0300

    Chave composta para Estoque Mensal

--- xfatur/src/main/java/com/xfatur/model/EstoqueMensal.java
+++ xfatur/src/main/java/com/xfatur/model/EstoqueMensal.java
@@ -4,36 +4,28 @@ import java.math.BigDecimal;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.xfatur.model.produto.Produto;
 
 @Entity
+@IdClass(EstoqueMensalId.class)
 public class EstoqueMensal {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private Integer quantidadeInicial;
     private Integer mes;
+    @Id
     private Integer ano;
-    private BigDecimal custoUnitario;
-
+    @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "produto_id")
     private Produto produto;
 
-    public Integer getId() {
-	return id;
-    }
-
-    public void setId(Integer id) {
-	this.id = id;
-    }
+    private Integer quantidadeInicial;
+    private BigDecimal custoUnitario;
 
     public Integer getMes() {
 	return mes;
