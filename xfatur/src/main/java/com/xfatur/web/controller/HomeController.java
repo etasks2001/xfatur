74fe8586c5933768058286bf66ac6b5324584572
satells <msergiost@hotmail.com>
2022-01-20 16:02:41 -0300

    Criação da página "home.html" e direcionamentos para demais páginas

--- xfatur/src/main/java/com/xfatur/web/controller/HomeController.java
+++ xfatur/src/main/java/com/xfatur/web/controller/HomeController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/index")
-public class IndexController {
+@RequestMapping({ "/", "/home" })
+public class HomeController {
 
     @GetMapping
     public String index() {
-	return "index";
+	return "home";
     }
 
 }
