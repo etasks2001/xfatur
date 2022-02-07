31605c96fc14397c0a5ff1437fccad8f055fdb59
satells <msergiost@hotmail.com>
2022-02-07 15:14:39 -0300

    .

--- xfatur/src/main/java/com/xfatur/web/controller/ClassificacaoFiscalController.java
+++ xfatur/src/main/java/com/xfatur/web/controller/ClassificacaoFiscalController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.xfatur.dto.produto.ClassificacaoFiscalDTO;
 import com.xfatur.service.produto.ClassificacaoFiscalService;
@@ -16,24 +17,26 @@ import com.xfatur.service.produto.ClassificacaoFiscalService;
 @RequestMapping("classificacaofiscal")
 public class ClassificacaoFiscalController {
 
-	@Autowired
-	private ClassificacaoFiscalService service;
+    @Autowired
+    private ClassificacaoFiscalService service;
 
-	@GetMapping("form")
-	public String openForm(ClassificacaoFiscalDTO dto) {
+    @GetMapping("form")
+    public String openForm(ClassificacaoFiscalDTO dto) {
 
-		return "/cadastro/classificacaofiscal";
+	return "/cadastro/classificacaofiscal";
+    }
+
+    @PostMapping("salvar")
+    public String salvar(@Valid ClassificacaoFiscalDTO classificacaoFiscal, BindingResult result, RedirectAttributes attr) {
+	if (result.hasErrors()) {
+	    return "cadastro/classificacaofiscal";
 	}
 
-	@PostMapping("salvar")
-	public String salvar(@Valid ClassificacaoFiscalDTO classificacaoFiscal, BindingResult result) {
-		if (result.hasErrors()) {
-			return "cadastro/classificacaofiscal";
-		}
+	service.save(classificacaoFiscal);
 
-		service.save(classificacaoFiscal);
+	attr.addFlashAttribute("success", "Classificação fiscal incluída.");
 
-		return "redirect:/classificacaofiscal/form";
-	}
+	return "redirect:/classificacaofiscal/form";
+    }
 
 }
