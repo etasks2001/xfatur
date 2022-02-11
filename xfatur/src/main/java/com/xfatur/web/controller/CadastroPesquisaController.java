05689fc392c64e9ef5eba9a93edb672b3ad1f76e
satells <msergiost@hotmail.com>
2022-02-11 16:53:05 -0300

    .

--- xfatur/src/main/java/com/xfatur/web/controller/CadastroPesquisaController.java
+++ xfatur/src/main/java/com/xfatur/web/controller/CadastroPesquisaController.java
@@ -2,14 +2,17 @@ package com.xfatur.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("cadastro")
 public class CadastroPesquisaController {
 
-    @GetMapping("pesquisar")
-    public String openForm() {
+    @GetMapping("pesquisar/{cadastro}")
+    public String openForm(@PathVariable("cadastro") String cadastro) {
+
+	System.out.println(cadastro);
 
 	return "/cadastro/pesquisa";
 
