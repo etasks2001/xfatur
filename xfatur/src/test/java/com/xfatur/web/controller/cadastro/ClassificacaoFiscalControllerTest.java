edaa3fa1c6f64eb91febbda69c165b9ef2669ac6
satells <msergiost@hotmail.com>
2022-03-21 16:49:45 -0300

    .

---
+++ xfatur/src/test/java/com/xfatur/web/controller/cadastro/ClassificacaoFiscalControllerTest.java
@@ -0,0 +1,17 @@
+package com.xfatur.web.controller.cadastro;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class ClassificacaoFiscalControllerTest {
+
+    @Test
+    void test() {
+	fail("Not yet implemented");
+    }
+
+}
