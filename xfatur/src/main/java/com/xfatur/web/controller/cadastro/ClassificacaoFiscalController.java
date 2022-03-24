5c33279d0b3c87a9a0094e3d7f0a6541e8e495ef
satells <msergiost@hotmail.com>
2022-03-24 15:30:35 -0300

    .

--- xfatur/src/main/java/com/xfatur/web/controller/cadastro/ClassificacaoFiscalController.java
+++ xfatur/src/main/java/com/xfatur/web/controller/cadastro/ClassificacaoFiscalController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,10 @@ public class ClassificacaoFiscalController {
     private ClassificacaoFiscalService service;
 
     @GetMapping("form")
-    public String openForm(ClassificacaoFiscalDTO dto) {
+    public String openForm(Model model) {
+	model.addAttribute("classificacaofiscal", new ClassificacaoFiscalDTO());
+
+	System.out.println(model.getClass().getCanonicalName());
 
 	return "/cadastro/classificacaofiscal";
     }
