e5140e8e2f55b300f373cf0d6e76e9ae4f3b9f66
satells <msergiost@hotmail.com>
2022-04-11 12:11:56 -0300

    .

--- xfatur/src/main/java/com/xfatur/XFaturApplication.java
+++ xfatur/src/main/java/com/xfatur/XFaturApplication.java
@@ -3,11 +3,13 @@ package com.xfatur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
+@EnableJpaRepositories(enableDefaultTransactions = false)
 public class XFaturApplication {
 
     public static void main(String[] args) {
